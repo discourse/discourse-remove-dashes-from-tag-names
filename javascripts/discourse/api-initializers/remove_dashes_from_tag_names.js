7208d566495fa68bc14b4cbc37ba3aadb054fedf
Faizaan Gagan <faizaan@discourse.org>
2023-11-16 11:31:07 +0530

    initial commit

---
+++ javascripts/discourse/api-initializers/remove_dashes_from_tag_names.js
@@ -0,0 +1,20 @@
+import { apiInitializer } from "discourse/lib/api";
+import { typeOf } from '@ember/utils';
+import { defaultRenderTag } from "discourse/lib/render-tag";
+
+export default apiInitializer("0.11.1", api => {
+  const customRenderer = (tag, params) => {
+    const result = defaultRenderTag(tag, params);
+    const text = $(result).html();
+    let modifiedText = text;
+    if (text && typeOf(text) == 'string') {
+        modifiedText =  text.replace(/-/g, " ");
+    }
+    
+    let val =  $(result).html(modifiedText).prop("outerHTML");
+    
+    return val;
+  }
+
+  api.replaceTagRenderer(customRenderer);
+});
