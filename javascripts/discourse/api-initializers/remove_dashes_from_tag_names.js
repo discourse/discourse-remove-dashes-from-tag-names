20e816484e62d73b4269a7ef719eb588da5c6301
David Taylor <david@taylorhq.com>
2024-03-27 17:45:37 +0000

    DEV: Update linting (#2)

--- javascripts/discourse/api-initializers/remove_dashes_from_tag_names.js
+++ javascripts/discourse/api-initializers/remove_dashes_from_tag_names.js
@@ -1,20 +1,21 @@
+import { typeOf } from "@ember/utils";
+import $ from "jquery";
 import { apiInitializer } from "discourse/lib/api";
-import { typeOf } from '@ember/utils';
 import { defaultRenderTag } from "discourse/lib/render-tag";
 
-export default apiInitializer("0.11.1", api => {
+export default apiInitializer("0.11.1", (api) => {
   const customRenderer = (tag, params) => {
     const result = defaultRenderTag(tag, params);
     const text = $(result).html();
     let modifiedText = text;
-    if (text && typeOf(text) == 'string') {
-        modifiedText =  text.replace(/-/g, " ");
+    if (text && typeOf(text) === "string") {
+      modifiedText = text.replace(/-/g, " ");
     }
-    
-    let val =  $(result).html(modifiedText).prop("outerHTML");
-    
+
+    let val = $(result).html(modifiedText).prop("outerHTML");
+
     return val;
-  }
+  };
 
   api.replaceTagRenderer(customRenderer);
 });
